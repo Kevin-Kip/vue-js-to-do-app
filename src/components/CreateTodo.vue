315c7cb280c95df25fb99233e1ff2db06913c884
Kevin-Kip <masterfork5@gmail.com>
2018-06-16 12:46:48 +0300

    Added the Insert Code

--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -1,12 +1,32 @@
 <template>
   <form>
-
+    <input type="text" v-bind="titleToSave"/><br>
+    <input type="text" v-bind="projectToSave"/><br>
+    <input type="text" v-bind="completedToSave"/><br>
+    <button class="ui button" v-on:click="saveTodo">Save</button>
   </form>
 </template>
 
 <script>
     export default {
-        name: "create-todo"
+        name: "create-todo",
+        props: ['todos'],
+        data() {
+          return {
+            titleToSave: null,
+            projectToSave: null,
+            completedToSave: null
+          }
+        },
+      methods: {
+          saveTodo() {
+              todos.push({
+                title: titleToSave,
+                project:  projectToSave,
+                completed: completedToSave
+              })
+          }
+      }
     }
 </script>
 
